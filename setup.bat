95c48587efcd2b02926dd8c1108d6549a1c1e955
Richard S. Stewart <rstewart2702@gmail.com>
2019-04-13 23:55:37 -0500

    More about what other python packages are needed:  flask-wtf...

--- setup.bat
+++ setup.bat
@@ -7,3 +7,7 @@ pip install flask-bootstrap
 rem At end of chapter 3, we add in flask-moment:
 pip install flask-moment
 
+rem At start of chapter 4, we add in flask-wtf, for help with
+rem forms handling:
+pip install flask-wtf
+
