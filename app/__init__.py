92920f1b04f0c84a06aa43d2e13f0f55317860a8
Richard S. Stewart <rstewart2702@gmail.com>
2019-07-09 16:09:08 -0500

    WORK-IN-PROGRESS:  just starting in chapter 7; this is the beginning of the package-constructor and factory-function features.  There is still more to do to resolve handing of the routes.

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+# application package constructor:
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+
+db = SQLAlchemy()
+
+# This is what Grinberg calls the "application factory function":
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    #
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment_init_app(app)
+    db.init_app(app)
+
+    # attach routes and custom error pages here.
+
+    return app
