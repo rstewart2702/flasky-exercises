e2963b6ea69a8f127e563bae115a039a945a58cf
Richard S. Stewart <rstewart2702@gmail.com>
2019-07-11 22:58:47 -0500

    Alternative config.py, with different setup for different locations.

--- config-1.py
+++ config-1.py
@@ -3,10 +3,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'mail.proassurance.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '25'))
     MAIL_USE_TLS = \
-      os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true','on','1']
+      os.environ.get('MAIL_USE_TLS', 'false').lower() in ['false','off','0']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
