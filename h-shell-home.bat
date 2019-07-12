e515de121ca60fcf9b3da12143be706c317b7cb0
Richard S. Stewart <rstewart2702@gmail.com>
2019-07-11 22:59:38 -0500

    Batch/shell files to start up command-line flask enviroments. Useful for troubleshooting.

---
+++ h-shell-home.bat
@@ -0,0 +1,9 @@
+set FLASK_APP=flasky.py
+set FLASK_DEBUG=1
+set FLASKY_ADMIN=flpy2702@gmail.com
+set MAIL_USERNAME=flpy2702@gmail.com
+
+set MAIL_USE_TLS=true
+
+flask shell
+
---
+++ h-shell.bat
@@ -0,0 +1,7 @@
+set FLASK_APP=flasky.py
+set FLASK_DEBUG=1
+set FLASKY_ADMIN=richardstewart@proassurance.com
+set MAIL_USERNAME=richardstewart@proassurance.com
+
+flask run
+
