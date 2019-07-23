b7a048e848fb7f48f80cd7245fe88cdecb4410ab
Richard S. Stewart <rstewart2702@gmail.com>
2019-07-23 16:37:02 -0500

    Added passwords to the User model.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from . import db
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 # Database-related classes:
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -17,6 +19,27 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), unique=True, index=True)
+    #
+    #
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    # Password hashing function is implemented through a "write-only"
+    # property called password:
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # Method verify_password takes a password and uses
+    # Werkzeug's check_password_hash for verification against hashed
+    # versoin stored in the User model.
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    
     #
     # for 'referential integrity' and ooey-gooey orm-ness:
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
