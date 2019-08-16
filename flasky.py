3d9b3b1eccf5f5ecbe8971103766e6a27600c00f
Richard S. Stewart <rstewart2702@gmail.com>
2019-08-16 14:47:54 -0500

    Added nicer startup messages for display when executing the "flask shell" command. The "db" object's attributes are "more available" in this kind of context, so the at least I can get some helpful diagnostic information about the currently chosen configuration and the current database url when I execute "flask shell" from the command line.

--- flasky.py
+++ flasky.py
@@ -6,8 +6,18 @@ from flask_migrate import Migrate
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app,db)
 
+# print("Current database url:  ", db.engine.url)
+
 @app.shell_context_processor
 def make_shell_context():
+    # These two execute successfully when you start up the flask "environment"
+    # at a command prompt, but they can't be used outside this context,
+    # for example, in the preceding lines, because the necessary object instances
+    # don't yet exist.  So, such output lines should be used elsewhere,
+    # perhaps inside of some part of the app initialization or something?
+    print("Running with configuration:  ",(os.getenv('FLASK_CONFIG') or 'default'))
+    print("Current database url:  ", db.engine.url)
+
     return dict(db=db, User=User, Role=Role)
 
 # The app.cli.command decorator allows us to implement
