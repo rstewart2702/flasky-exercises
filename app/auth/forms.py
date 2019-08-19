c6ed69b8ab0efd615a5f54b04e34991a936bf0c5
Richard S. Stewart <rstewart2702@gmail.com>
2019-08-19 12:04:11 -0500

    Correction to the name of the "Length" validator!

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -30,11 +30,11 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Log In')
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), length(1,64), Email()])
+    email = StringField('Email', validators=[DataRequired(), Length(1,64), Email()])
     
     username = \
         StringField('Username',
-                    validators = [DataRequired(), length(1,64),
+                    validators = [DataRequired(), Length(1,64),
                                   Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                          'Usernames must have only letters, numbers, dots or '
                                          'underscores')])
