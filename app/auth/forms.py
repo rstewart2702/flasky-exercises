72239f8177fee758c4f940141c5f2769a1d0523a
Richard S. Stewart <rstewart2702@gmail.com>
2019-08-19 11:32:51 -0500

    Changes to allow a new user to register.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
 
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+
+from wtforms import ValidationError
+from ..models import User
 
 class LoginForm(FlaskForm):
     # This email field uses the Length() and Email() validators
@@ -25,3 +28,32 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Keep me logged in')
     #
     submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), length(1,64), Email()])
+    
+    username = \
+        StringField('Username',
+                    validators = [DataRequired(), length(1,64),
+                                  Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                         'Usernames must have only letters, numbers, dots or '
+                                         'underscores')])
+    password = \
+        PasswordField('Password',
+                      validators = [DataRequired(),
+                                    EqualTo('password2', message='Passwords must match.')])
+    password2 = \
+        PasswordField('Confirm Password',
+                      validators = [DataRequired()])
+    submit = SubmitField('Register')
+
+    # Any method which has name which starts with "validate_" is invoked
+    # via reflection features of Python in order to perform additional
+    # per-field validation.
+    def validate_email(self, field):
+        if User.query.filter_by(email = field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username = field.data).first():
+            raise ValidationError('Username already in use.')
