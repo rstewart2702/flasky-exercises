d48e67914f2da20cd83e75891120522dfe6db049
Richard S. Stewart <rstewart2702@gmail.com>
2019-08-27 15:33:32 -0500

    Wow, keep on hitting things I had overlooked as I followed Grinberg's narrative in Chapter 8.  Still, this seems to be more instructive than copying everything over from his "example" git repository without trying to think and reason some things through.
    Another instructive thing:  this has been an education in how to write import
    statements which correctly refer to different parts of the hierarchical package
    namespaces provided within Python.

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, login_required, logout_user
 
 from . import auth
+from .. import db
 
 from ..models import User
 from .forms import LoginForm, RegistrationForm
